98e43256eab9a29cbe190ee33205d717c9cf6b15
Peter Schröder <phoetmail@googlemail.com>
2013-09-07 11:38:23 +0200

    initial import with ember bootstrap and no db

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Live::Application.config.secret_key_base = '6b43fbe254ba105353deb821cf7c8fc9f5ae6b4e0b3af4f064dce82903c27b3f690186b1a5ccf21cc4e015b5aea97e611e6d67729edea46e9166667410362d39'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Live::Application.routes.draw do
+  root 'home#index'
+end
