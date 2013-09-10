114bde139351d40c490d084d821cff5c5c54c416
Peter Schröder <phoetmail@googlemail.com>
2013-09-10 16:37:41 +0200

    add nextUp example

--- app/assets/javascripts/controllers/application_controller.js
+++ app/assets/javascripts/controllers/application_controller.js
@@ -1,4 +1,9 @@
 Live.ApplicationController = Ember.Controller.extend({});
 
 Live.IndexController = Ember.ArrayController.extend({
+  nextUp: function() {
+    return this.filter(function(song) {
+      return true;
+    });
+  }.property('@each')
 });
