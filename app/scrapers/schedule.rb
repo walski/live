a5dba267fb210ef33ab644a897eae598da409e80
Peter Schröder <phoetmail@googlemail.com>
2013-09-19 14:31:24 +0200

    fetch via internet

--- app/scrapers/schedule.rb
+++ app/scrapers/schedule.rb
@@ -8,8 +8,8 @@ class Schedule
   end
 
   def fetch
-    # @data = Curl.get(URL).body_str
-    @data = File.open(Rails.root.join("config/schedule.json"), 'r') { |f| f.read }
+    @data = Curl.get(URL).body_str
+    # @data = File.open(Rails.root.join("config/schedule.json"), 'r') { |f| f.read }
   end
 
   def parse
