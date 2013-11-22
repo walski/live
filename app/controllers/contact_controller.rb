662ac5a895000b49e40cf661ea0ff79054af75bd
Thorben Schröder <stillepost@gmail.com>
2013-11-22 01:27:55 +0100

    Makes nice redirects for The Preface badge QR codes

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,5 +1,17 @@
 class ContactController < ApplicationController
   def show
-    render text: params[:id]
+    participant = Participant.where(:qr_code => params[:id]).first
+
+    unless participant
+      render text: "Badge code not found"
+      return
+    end
+
+    if !participant.profile_url || participant.profile_url.blank?
+      render text: "Sorry, this attendee has no XING profile"
+      return
+    end
+
+    redirect_to participant.profile_url
   end
 end
